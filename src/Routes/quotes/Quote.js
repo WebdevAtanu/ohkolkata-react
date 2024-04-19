936a2154dd00788adedf9ee827bb46f3e63fea42
Atanu Mondal <138792317+WebdevAtanu@users.noreply.github.com>
2024-04-19 15:32:51 +0530

    Update Quote.js

--- src/Routes/quotes/Quote.js
+++ src/Routes/quotes/Quote.js
@@ -11,38 +11,38 @@ return(
 			<div className="row g-2">
 				<div className="col-md-4">
 					<div className="container quotes">
-						<p><q>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum maiores culpa accusantium sunt. Voluptates, quo officia alias officiis, neque iusto consequuntur, sit provident est porro pariatur facere eveniet temporibus eius.</q></p>
-						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Lorem ipsum dolor</h5>
+						<p><q>Calcutta is the only city I know where you are actively encouraged to stop strangers at random for a quick chat.</q></p>
+						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Tahir Shah</h5>
 					</div>
 				</div>
 				<div className="col-md-4">
 					<div className="container quotes">
-						<p><q>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum maiores culpa accusantium sunt. Voluptates, quo officia alias officiis, neque iusto consequuntur, sit provident est porro pariatur facere eveniet temporibus eius.</q></p>
-						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Lorem ipsum dolor</h5>
+						<p><q>Where does one go in a tremendous city like Calcutta to find insider information? I recalled India’s golden rule: do the opposite of what would be normal anywhere else.</q></p>
+						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>V.S Naipul</h5>
 					</div>
 				</div>
 				<div className="col-md-4">
 					<div className="container quotes">
-						<p><q>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum maiores culpa accusantium sunt. Voluptates, quo officia alias officiis, neque iusto consequuntur, sit provident est porro pariatur facere eveniet temporibus eius.</q></p>
-						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Lorem ipsum dolor</h5>
+						<p><q>Calcutta is not for everyone. You want your city clean and green, stick to Delhi. You want your city rich and impersonal, go to Bombay. You want them hi-tech and full of draught beer, Bangalore’s your place. But if you want a city with a soul, come to Calcutta.</q></p>
+						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Vir Sanghvi</h5>
 					</div>
 				</div>
 				<div className="col-md-4">
 					<div className="container quotes">
-						<p><q>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum maiores culpa accusantium sunt. Voluptates, quo officia alias officiis, neque iusto consequuntur, sit provident est porro pariatur facere eveniet temporibus eius.</q></p>
-						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Lorem ipsum dolor</h5>
+						<p><q>Calcutta is like another world. People, there are very special and grateful.</q></p>
+						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Penelope Cruz</h5>
 					</div>
 				</div>
 				<div className="col-md-4">
 					<div className="container quotes">
-						<p><q>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum maiores culpa accusantium sunt. Voluptates, quo officia alias officiis, neque iusto consequuntur, sit provident est porro pariatur facere eveniet temporibus eius.</q></p>
-						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Lorem ipsum dolor</h5>
+						<p><q>Kolkata always opens its heart to me and I am a sucker for love.</q></p>
+						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Vidya Balan</h5>
 					</div>
 				</div>
 				<div className="col-md-4">
 					<div className="container quotes">
-						<p><q>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum maiores culpa accusantium sunt. Voluptates, quo officia alias officiis, neque iusto consequuntur, sit provident est porro pariatur facere eveniet temporibus eius.</q></p>
-						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Lorem ipsum dolor</h5>
+						<p><q>Those places where sadness and misery abound are favored settings for stories of ghosts and apparitions. Calcutta has countless such stories hidden in its darkness.</q></p>
+						<h5 className="text-end mt-4 text-light"><img src="image/quote/man1.jpg" alt=""/>Carlos Ruiz Zafon</h5>
 					</div>
 				</div>
 			</div>
